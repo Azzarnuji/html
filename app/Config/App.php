aaec90035d491b385887e5b4c5d197d00262082f
Azzarnuzy <azzarnuzy93@gmail.com>
2021-12-18 17:49:01 +0700

    eidt app

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'https://pontrendarulmuttaqin.com/';
+    public $baseURL = 'http://34.101.218.110';
 
     /**
      * --------------------------------------------------------------------------
